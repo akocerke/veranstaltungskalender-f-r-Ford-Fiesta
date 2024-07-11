46cafcd4cf3a2a852bc6370a9bb3b2a0f3951e80
akocerke <152269613+akocerke@users.noreply.github.com>
2024-07-11 11:46:36 +0200

    Navbar erstellt

--- app/src/components/Header.js
+++ app/src/components/Header.js
@@ -1,13 +1,16 @@
-import React from 'react';
-import logo from '../images/logo.png';
+import React from "react";
+import logo from "../images/logo.png";
+import Navbar from "./Navbar";
 
 const HeroSection = () => (
-  
-    <div className='Header text-center'>
-      <h1 className='mt-3 headline'>Ford Fiesta Treffen & Events</h1>
-      <img src={logo} alt="Logo" height='100' />
-      <hr className='hr1 shadow-lg'/>
+  <div className="Header text-center sticky-top">
+    <Navbar />
+    <div>
+      <h1 className="mt-3 headline">Ford Fiesta Treffen & Events</h1>
+      <img src={logo} alt="Logo" height="100" />
+      <hr className="hr1 shadow-lg" />
     </div>
+  </div>
 );
 
 export default HeroSection;
