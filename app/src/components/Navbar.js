834f381545c854edb25493361e5b4a57cf2733d9
akocerke <152269613+akocerke@users.noreply.github.com>
2024-08-06 10:01:38 +0200

    update navbar to Link

--- app/src/components/Navbar.js
+++ app/src/components/Navbar.js
@@ -1,12 +1,13 @@
 // Navbar.js
 import React from 'react'
 import { Navbar, Nav, Container } from 'react-bootstrap'
+import { Link } from 'react-router-dom';
 import logo from '../images/logo.png'
 
 const NavigationBar = () => (
   <Navbar className="bg-nav fixed-top" variant="dark" expand="lg">
     <Container>
-      <Navbar.Brand href="/">
+      <Navbar.Brand as={Link} to="/">
         <img
           src={logo}
           height="40"
@@ -17,25 +18,25 @@ const NavigationBar = () => (
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="mx-auto">
-          <Nav.Link className="me-3" href="/">
+          <Nav.Link className="me-3" as={Link} to="/">
             <i className="bi bi-house-door p-1"></i>Home
           </Nav.Link>
-          <Nav.Link className="me-3" href="#">
+          <Nav.Link className="me-3" as={Link} to="#">
             <i className="bi bi-people p-1"></i>Profil
           </Nav.Link>
-          <Nav.Link className="me-3" href="#">
+          <Nav.Link className="me-3" as={Link} to="#">
             <i className="bi bi-calendar-event p-1"></i>Events
           </Nav.Link>
         </Nav>
 
         <Nav>
-          <Nav.Link href="#">
+          <Nav.Link as={Link} to="#">
             <i className="bi bi-box-arrow-in-left p-1"></i>Login
           </Nav.Link>
-          <Nav.Link href="#">
+          <Nav.Link as={Link} to="#">
             <i className="bi bi-person-plus p-1"></i>Register
           </Nav.Link>
-          <Nav.Link href="#">
+          <Nav.Link as={Link} to="#">
             <i className="bi bi-box-arrow-in-right p-1"></i>Logout
           </Nav.Link>
         </Nav>
